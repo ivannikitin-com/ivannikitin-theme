93aef0b74f8e2800d2bf824c326753c09469dc19
Vadim Mishkhozhev <vadim.stug@gmail.com>
2019-01-21 14:16:39 +0700

    add minimize

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+
+const DEV = process.env.NODE_ENV === 'development';
 
 module.exports = {
   entry: './assets/app/js/index.js',
@@ -7,13 +15,63 @@ module.exports = {
     filename: './public/js/script.min.js',
     publicPath: 'assets/'
   },
+  devtool: DEV ? 'cheap-eval-source-map' : 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
         // exclude: /node_modules/
+      },
+      {
+        test: /\.(scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [
+                  autoprefixer({
+                    grid: "autoplace",
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ],
+                  }),
+                ],
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('public/css/style.min.css'),
+  ],
+  optimization: {
+    minimizer: [
+      !DEV &&
+      new UglifyJsPlugin({
+        sourceMap: true,
+      }),
+      !DEV &&
+      new OptimizeCSSAssetsPlugin()
     ]
   }
 }
