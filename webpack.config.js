499646e9a955d5fcca36c68175c85dc226ba0557
Vadim Mishkhozhev <vadim.stug@gmail.com>
2019-01-21 15:27:01 +0700

    add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,29 @@ module.exports = {
             }
           ]
         })
-      }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'public/images'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'public/fonts'
+            }
+        }]
+      },
     ]
   },
   plugins: [
