637003eb7da888ceea562265c34ebe818f4fcea9
Vadim Mishkhozhev <vadim.stug@gmail.com>
2019-03-19 15:42:44 +0700

    change page template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,43 +10,18 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<?php in_2019_post_thumbnail(); ?>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
+			</div>
+		</div>
+	</div>
 
 	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'in-2019' ),
-			'after'  => '</div>',
-		) );
-		?>
+		<?php the_content(); ?>
 	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'in-2019' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+	
 </article><!-- #post-<?php the_ID(); ?> -->
