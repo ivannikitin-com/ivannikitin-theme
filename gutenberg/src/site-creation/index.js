c0f87ae830d83642535ea4840df2d4d47484d84b
Vadim Mishkhozhev <vadim.stug@gmail.com>
2019-03-20 17:53:10 +0700

    remove tabs

--- gutenberg/src/site-creation/index.js
+++ gutenberg/src/site-creation/index.js
@@ -1,6 +1,5 @@
 import './style.scss';
 import './editor.scss';
-import Tabs from './components/tabs';
 
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
@@ -12,36 +11,10 @@ registerBlockType( 'in-2019/site-creation', {
 		return (
 			<div>
 				<h1>Tabs Demo</h1>
-				<Tabs>
-					<div label="Gator">
-						See ya later, <em>Alligator</em>!
-					</div>
-					<div label="Croc">
-						After while, <em>Crocodile</em>!
-					</div>
-					<div label="Sarcosuchus">
-						Nothing to see here, this tab is <em>extinct</em>!
-					</div>
-				</Tabs>
 			</div>
 		);
 	},
 	save: () => {
-		return (
-			<div>
-				<h1>Tabs Demo</h1>
-				<Tabs>
-					<div label="Gator">
-						See ya later, <em>Alligator</em>!
-					</div>
-					<div label="Croc">
-						After while, <em>Crocodile</em>!
-					</div>
-					<div label="Sarcosuchus">
-						Nothing to see here, this tab is <em>extinct</em>!
-					</div>
-				</Tabs>
-			</div>
-		);
+		return null;
 	},
 } );
