114baab5672b47e0be49066a11a11d5b6ffa2beb
Вадим Мишхожев <vadim.stug@gmail.com>
2019-11-06 13:30:22 +0700

    Обновлен дизайн шапки

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -34,7 +34,7 @@ function in_2019_woocommerce_scripts()
 	$version = wp_get_theme()->get('Version');
 	wp_enqueue_style(
 		'in-2019-woocommerce-style',
-		get_template_directory_uri() . '/woocommerce.css',
+		get_template_directory_uri() . '/dist/woocommerce.css',
 		array(),
 		$version
 	);
@@ -218,34 +218,6 @@ if (!function_exists('in_2019_woocommerce_cart_link_fragment')) {
 }
 add_filter('woocommerce_add_to_cart_fragments', 'in_2019_woocommerce_cart_link_fragment');
 
-if (!function_exists('in_2019_woocommerce_cart_link')) {
-	/**
-	 * Cart Link.
-	 *
-	 * Displayed a link to the cart including the number of items present and the cart total.
-	 *
-	 * @return void
-	 */
-	function in_2019_woocommerce_cart_link()
-	{
-		?>
-		<a class="cart-contents" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php esc_attr_e(
-	'View your shopping cart',
-	'in-2019'
-); ?>">
-			<?php $item_count_text = sprintf(
-   	/* translators: number of items in the mini cart. */
-   	_n('%d item', '%d items', WC()->cart->get_cart_contents_count(), 'in-2019'),
-   	WC()->cart->get_cart_contents_count()
-   ); ?>
-			<span class="amount"><?php echo wp_kses_data(
-   	WC()->cart->get_cart_subtotal()
-   ); ?></span> <span class="count"><?php echo esc_html($item_count_text); ?></span>
-		</a>
-		<?php
-	}
-}
-
 if (!function_exists('in_2019_woocommerce_header_cart')) {
 	/**
 	 * Display Header Cart.
@@ -254,24 +226,19 @@ if (!function_exists('in_2019_woocommerce_header_cart')) {
 	 */
 	function in_2019_woocommerce_header_cart()
 	{
-		if (is_cart()) {
-			$class = 'current-menu-item';
-		} else {
-			$class = 'nodecor';
-		} ?>
-		<ul id="site-header-cart" class="site-header__cart">
-			<li class="<?php echo esc_attr($class); ?>">
-				<?php in_2019_woocommerce_cart_link(); ?>
-			</li>
-			<li>
-				<?php
-    $instance = array(
-    	'title' => ''
-    );
-
-    the_widget('WC_Widget_Cart', $instance);?>
-			</li>
-		</ul>
+		?>
+		<div id="site-header-cart" class="site-header__cart">
+      <a class="site-header__cart-amount" href="<?php echo esc_url(
+      	wc_get_cart_url()
+      ); ?>" title="<?php esc_attr_e('View your shopping cart', 'in-2019'); ?>">
+        <span class="site-header__cart-icon">
+          <i class="fas fa-shopping-cart"></i>
+        </span>
+        <span class="site-header__cart-count">
+          <?php echo WC()->cart->get_cart_contents_count(); ?>
+        </span>
+      </a>
+		</div>
 		<?php
 	}
 }
--- src/js/icons.js
+++ src/js/icons.js
@@ -8,8 +8,19 @@ import {
 	faEnvelope,
 	faSearch,
 	faStream,
+	faShoppingCart,
 } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faPhoneAlt, faTable, faTasks, faProjectDiagram, faUser, faEnvelope, faSearch, faStream);
+library.add(
+	faPhoneAlt,
+	faTable,
+	faTasks,
+	faProjectDiagram,
+	faUser,
+	faEnvelope,
+	faSearch,
+	faStream,
+	faShoppingCart,
+);
 
 dom.i2svg();
