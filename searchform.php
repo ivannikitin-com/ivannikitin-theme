18dfe6a79fdbef4b9885bc7f93133abaea71184f
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-02-02 21:43:02 +0700

    Добавлены стили для  магазина

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form action="/" method="get" class="site-header__search-from form-row">
     <i class="fas fa-search"></i>
     <input class="site-header__search-field col order-2" type="text" name="s" placeholder="" value="">
-    <input type="submit" value="" class="site-header__search-button order-1">
+    <input type="submit" value="" class="site-header__search-button order-1 d-none">
 </form>
