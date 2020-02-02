a503a3efd91f6ae035735bd6733f2ada36c7ad86
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-02-02 22:55:17 +0700

    fix grid

--- archive-jetpack-portfolio.php
+++ archive-jetpack-portfolio.php
@@ -26,21 +26,23 @@ get_header();
 							the_archive_description( '<div class="archive-description">', '</div>' );
 							?>
 						</header><!-- .page-header -->
+						<div class="row">
+							<?php
+							/* Start the Loop */
+							while ( have_posts() ) :
+								the_post();
 
+								/*
+								* Include the Post-Type-specific template for the content.
+								* If you want to override this in a child theme, then include a file
+								* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+								*/
+								get_template_part( 'template-parts/blog/content', 'portfolio' );
+
+							endwhile;
+							?>
+						</div>
 						<?php
-						/* Start the Loop */
-						while ( have_posts() ) :
-							the_post();
-
-							/*
-							* Include the Post-Type-specific template for the content.
-							* If you want to override this in a child theme, then include a file
-							* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-							*/
-							get_template_part( 'template-parts/blog/content', 'portfolio' );
-
-						endwhile;
-
 						the_posts_navigation();
 
 						else :
